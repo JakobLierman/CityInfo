980baf4855820f9d76f28936f39d1f0d56da349e
Jakob Lierman <jakob.lierman@gmail.com>
2018-05-02 01:02:18 +0200

    BerichtToevoegen component

--- src/app/bericht-toevoegen/bericht-toevoegen.component.ts
+++ src/app/bericht-toevoegen/bericht-toevoegen.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-bericht-toevoegen',
-  templateUrl: './bericht-toevoegen.component.html',
-  styleUrls: ['./bericht-toevoegen.component.css']
+  selector: "app-bericht-toevoegen",
+  templateUrl: "./bericht-toevoegen.component.html",
+  styleUrls: ["./bericht-toevoegen.component.css"]
 })
 export class BerichtToevoegenComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  berichtToevoegen(berichtTitel: HTMLInputElement, berichtTekst: HTMLInputElement): boolean {
+    console.log(berichtTitel.value);
+    console.log(berichtTekst.value);
+    console.log();
+    return false;
   }
-
 }
