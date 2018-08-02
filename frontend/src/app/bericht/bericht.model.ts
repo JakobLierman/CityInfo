9a004c6a023ba02ec399818792e0dfdb9c0d4e25
Jakob Lierman <jakob.lierman@gmail.com>
2018-08-02 11:03:43 +0200

    Aanpassen bericht

--- frontend/src/app/bericht/bericht.model.ts
+++ frontend/src/app/bericht/bericht.model.ts
@@ -1,14 +1,20 @@
+import { Observable } from 'rxjs/observable';
+import { Reactie } from './reactie/reactie.model';
+
 export class Bericht {
   private _id: number;
   private _titel: string;
-  private _bericht: string;
-  private _dateAdded: Date = new Date();
+  private _boodschap: string;
   private _categorie: string;
+  private _user: string; // Should be user
+  private _dateAdded: Date = new Date();
+  private _reacties: Observable<Reactie>;
 
-  constructor(titel: string, bericht: string, categorie: string, dateAdded: Date = null) {
+  constructor(titel: string, boodschap: string, categorie: string, user: string, dateAdded: Date = null) {
     this._titel = titel;
-    this._bericht = bericht;
+    this._boodschap = boodschap;
     this._categorie = categorie;
+    this._user = user;
     this._dateAdded = dateAdded ? dateAdded : new Date();
   }
 
@@ -20,15 +26,23 @@ export class Bericht {
     return this._titel;
   }
 
-  get bericht(): string {
-    return this._bericht;
+  get boodschap(): string {
+    return this._boodschap;
   }
 
   get category(): string {
     return this._categorie;
   }
 
+  get user(): string {
+    return this._user;
+  }
+
   get dateAdded(): Date {
-      return this._dateAdded;
+    return this._dateAdded;
+  }
+
+  get reacties(): Observable<Reactie> {
+    return this._reacties;
   }
 }
--- frontend/src/app/bericht/bericht/bericht.component.ts
+++ frontend/src/app/bericht/bericht/bericht.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { Bericht } from '../bericht.model';
 
 @Component({
@@ -7,9 +7,14 @@ import { Bericht } from '../bericht.model';
   styleUrls: ['./bericht.component.css']
 })
 export class BerichtComponent implements OnInit {
-  @Input() bericht: Bericht;
+  @Input() public bericht: Bericht;
+  @Output() public deleteBericht = new EventEmitter<Bericht>();
 
   constructor() {}
 
   ngOnInit() {}
+
+  removeBericht() {
+    this.deleteBericht.emit(this.bericht);
+  }
 }
