0a3bcb21a1e5d21b07d2ef056291d0b80141a34f
Jakob Lierman <jakob.lierman@gmail.com>
2018-08-04 00:47:25 +0200

    Uitwerken RegisterComponent

--- frontend/src/app/user/register/register.component.ts
+++ frontend/src/app/user/register/register.component.ts
@@ -1,4 +1,23 @@
+import { Observable } from 'rxjs/observable';
+import { AuthenticationService } from '../authentication.service';
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, Validators, FormBuilder, ValidatorFn, AbstractControl } from '@angular/forms';
+import { Router } from '@angular/router';
+import { map } from 'rxjs/operators';
+
+function passwordValidator(length: number): ValidatorFn {
+  return (control: AbstractControl): { [key: string]: any } => {
+    return control.value.length < length ? { 'passwordTooShort': {
+      requiredLength: length, actualLength: control.value.length
+    } } : null;
+  };
+}
+
+function comparePasswords(control: AbstractControl): { [key: string]: any } {
+  const password = control.get('password');
+  const confirmPassword = control.get('confirmPassword');
+  return password.value === confirmPassword.value ? null : { 'passwordsDiffer': true };
+}
 
 @Component({
   selector: 'app-register',
@@ -6,10 +25,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
+  public user: FormGroup;
+  public errorMsg: string;
 
-  constructor() { }
+  constructor(private authenticationService: AuthenticationService,
+    private router: Router,
+    private fb: FormBuilder) { }
 
   ngOnInit() {
+    this.user = this.fb.group({
+      username: ['', [Validators.required, Validators.minLength(4)],
+    this.serverSideValidateUsername()],
+    passwordGroup: this.fb.group({
+      password: ['', [Validators.required, passwordValidator(12)]],
+      confirmPassword: ['', Validators.required]
+    }, { validator: comparePasswords })
+    });
   }
 
+  serverSideValidateUsername(): ValidatorFn {
+    return (control: AbstractControl): Observable<{ [key: string]: any }> => {
+      return this.authenticationService.checkUsernameAvailability(control.value).pipe(
+        map(available => {
+          if (available) {
+            return null;
+          }
+          return { userAlreadyExists: true };
+        })
+      );
+    };
+  }
 }
