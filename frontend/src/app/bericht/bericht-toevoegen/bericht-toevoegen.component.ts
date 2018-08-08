357ccac8de12eb6092c81f086a6de494b258fc74
Jakob Lierman <jakob.lierman@gmail.com>
2018-08-08 16:40:44 +0200

    Bijwerken BerichtToevoegenComponent

--- frontend/src/app/bericht/bericht-toevoegen/bericht-toevoegen.component.ts
+++ frontend/src/app/bericht/bericht-toevoegen/bericht-toevoegen.component.ts
@@ -1,19 +1,59 @@
-import { Component, OnInit } from "@angular/core";
+import { HttpErrorResponse } from '@angular/common/http';
+import { BerichtDataService } from './../bericht-data.service';
+import { FormGroup, FormBuilder, Validators, FormArray } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { Bericht, Categorie } from '../bericht.model';
 
 @Component({
-  selector: "app-bericht-toevoegen",
-  templateUrl: "./bericht-toevoegen.component.html",
-  styleUrls: ["./bericht-toevoegen.component.css"]
+  selector: 'app-bericht-toevoegen',
+  templateUrl: './bericht-toevoegen.component.html',
+  styleUrls: ['./bericht-toevoegen.component.css']
 })
 export class BerichtToevoegenComponent implements OnInit {
-  constructor() {}
+  bericht: FormGroup;
+  errorMsg: string;
 
-  ngOnInit() {}
+  private _categorieen: Categorie[];
 
-  berichtToevoegen(berichtTitel: HTMLInputElement, berichtTekst: HTMLInputElement): boolean {
-    console.log(berichtTitel.value);
-    console.log(berichtTekst.value);
-    console.log();
-    return false;
+  constructor(
+    private fb: FormBuilder,
+    private _berichtDataServive: BerichtDataService
+  ) {}
+
+  ngOnInit() {
+    this._berichtDataServive.categorieen.subscribe(
+      categorieen => (this._categorieen = categorieen),
+      (error: HttpErrorResponse) => {
+        this.errorMsg = `Error ${error.status} while trying to recieve Categorieen: ${error.error}`;
+      }
+    );
+    this.bericht = this.fb.group({
+      titel: ['', [Validators.required, Validators.minLength(2)]],
+      boodschap: ['', [Validators.required, Validators.minLength(25)]],
+      categorie: ['', [Validators.required]]
+    });
+  }
+
+  get categorieen() {
+    return this._categorieen;
+  }
+
+  onSubmit() {
+    const bericht = new Bericht(
+      this.bericht.value.titel,
+      this.bericht.value.boodschap,
+      this.bericht.value.categorie
+    );
+
+    this._berichtDataServive.berichtToevoegen(bericht).subscribe(
+      () => {
+        this.bericht.reset();
+      },
+      (error: HttpErrorResponse) => {
+        this.errorMsg = `Error ${error.status} while adding Bericht for ${
+          bericht.titel
+        }: ${error.error}`;
+      }
+    );
   }
 }
