9481c5578e5849f01494d0c9ca064c1085032224
Jakob Lierman <jakob.lierman@gmail.com>
2018-08-09 14:59:29 +0200

    Toevoegen UserModel

---
+++ frontend/src/app/user/user.model.ts
@@ -0,0 +1,75 @@
+export class Regio {
+    private _id: number;
+    private _naam: string;
+
+    constructor(naam: string) {
+        this._naam = naam;
+    }
+
+    get id(): number {
+        return this._id;
+    }
+
+    get naam(): string {
+        return this._naam;
+    }
+}
+
+export class User {
+    private _id: number;
+    private _username: string;
+    private _voornaam: string;
+    private _familienaam: string;
+    private _email: string;
+    private _regio: Regio;
+    // private _hash: string;
+    // private _salt: string;
+
+    constructor(username: string, voornaam: string, familienaam: string, email: string, regio: Regio) {
+        this._username = username;
+        this._voornaam = voornaam;
+        this._familienaam = familienaam;
+        this._email = email;
+        this._regio = regio;
+    }
+
+    get id(): number {
+        return this._id;
+    }
+
+    get username(): string {
+        return this._username;
+    }
+
+    set username(username: string) {
+        this._username = username;
+    }
+    
+    get voornaam(): string {
+        return this._voornaam;
+    }
+
+    get familienaam(): string {
+        return this._familienaam;
+    }
+
+    get naam(): string {
+        return this._voornaam + ' ' + this._familienaam;
+    }
+
+    get email(): string {
+        return this._email;
+    }
+
+    set email(email: string) {
+        this._email = email;
+    }
+
+    get regio(): Regio {
+        return this._regio;
+    }
+
+    set regio(regio: Regio) {
+        this._regio = regio;
+    }
+}
