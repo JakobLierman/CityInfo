b12e11771ea3a6e8a47ca43ac45046556e19594b
Jakob Lierman <jakob.lierman@gmail.com>
2018-08-10 16:26:33 +0200

    Toevoegen UserDataService

---
+++ frontend/src/app/user/user-data.service.ts
@@ -0,0 +1,32 @@
+import {Injectable} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs/Observable";
+import {Regio} from "./user.model";
+import {map} from "rxjs/operators";
+
+@Injectable()
+export class UserDataService {
+  private readonly _appUrl = '/API';
+
+  constructor(private http: HttpClient) {
+  }
+
+  // Regio's
+  get regios(): Observable<Regio[]> {
+    return this.http
+      .get(`{this._appUrl}/regios/`)
+      .pipe(map((list: any[]): Regio[] => list.map(Regio.fromJSON)));
+  }
+
+  regioToevoegen(regio: Regio): Observable<Regio> {
+    return this.http
+      .post(`${this._appUrl}/regios/`, regio)
+      .pipe(map(Regio.fromJSON));
+  }
+
+  regioVerwijderen(regio: Regio): Observable<Regio> {
+    return this.http
+      .delete(`${this._appUrl}/regio/${regio.id}`)
+      .pipe(map(Regio.fromJSON));
+  }
+}
