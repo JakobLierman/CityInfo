66da9dbe6a745b3a01e07c441776bb875679048a
Jakob Lierman <jakob.lierman@gmail.com>
2018-08-12 22:15:28 +0200

    Klaarmaken voor Heroku
    Poging #387897465 ;-)

--- serve-frontend/index.js
+++ serve-frontend/index.js
@@ -2,13 +2,13 @@
 
 var express = require('express');
 var app = express();
-var fs = require('fs');
+var fs = require("fs");
 var config;
 
 app.listen(process.env.PORT, function() {
-	console.log("Server running on port 8080.");
+  console.log("Server running on port 8080.");
 });
 
 // Serve www folder
-app.use(express.static(__dirname + 'www'));
-app.use('*', express.static(__dirname / 'www/index.html'));
+app.use(express.static(__dirname + '/www/'));
+app.use('*', express.static(__dirname + '/www/index.html'));
