297edc2af8db6ac1d5257e1f17eb09d36dc42567
Jakob Lierman <jakob.lierman@gmail.com>
2018-08-13 18:01:30 +0200

    Afwerken BerichtFilterPipe

--- frontend/src/app/bericht/bericht-filter.pipe.ts
+++ frontend/src/app/bericht/bericht-filter.pipe.ts
@@ -1,17 +1,19 @@
 import {Bericht} from './bericht.model';
 import {Pipe, PipeTransform} from '@angular/core';
-import {Regio} from "../user/user.model";
 
 @Pipe({
   name: 'berichtFilter'
 })
 export class BerichtFilterPipe implements PipeTransform {
 
-  transform(berichten: Bericht[], regio: Regio, titel: string): Bericht[] {
-    berichten.filter(rec => rec.user.regio === regio);
-    if ((!titel || titel.length === 0) && !regio) {
+  transform(berichten: Bericht[], regio: string, titel: string): Bericht[] {
+    if (!berichten) {
+      return [];
+    }
+    if (!titel || titel.length === 0) {
       return berichten;
     }
-    return berichten.filter(ber => ber.titel.toLowerCase().startsWith(titel.toLowerCase()));
+    const regioFilter = berichten.filter(ber => ber.user.regio === regio);
+    return regioFilter.filter(ber => ber.titel.toLowerCase().startsWith(titel.toLowerCase()));
   }
 }
