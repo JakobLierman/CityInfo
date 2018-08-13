92fb6fb85c1fdb0ef3d515d2b9f4e97636d5a004
Jakob Lierman <jakob.lierman@gmail.com>
2018-08-13 13:11:20 +0200

    Toevoegen pagination + aanpassen BerichtFilter

--- frontend/src/app/bericht/bericht-filter.pipe.ts
+++ frontend/src/app/bericht/bericht-filter.pipe.ts
@@ -1,16 +1,17 @@
-import { Bericht } from './bericht.model';
-import { Pipe, PipeTransform } from '@angular/core';
+import {Bericht} from './bericht.model';
+import {Pipe, PipeTransform} from '@angular/core';
+import {Regio} from "../user/user.model";
 
 @Pipe({
   name: 'berichtFilter'
 })
 export class BerichtFilterPipe implements PipeTransform {
 
-  transform(berichten: Bericht[], titel: string): Bericht[] {
-    if (!titel || titel.length === 0) {
+  transform(berichten: Bericht[], regio: Regio, titel: string): Bericht[] {
+    berichten.filter(rec => rec.user.regio === regio);
+    if ((!titel || titel.length === 0) && !regio) {
       return berichten;
     }
     return berichten.filter(rec => rec.titel.toLowerCase().startsWith(titel.toLowerCase()));
   }
-
 }
