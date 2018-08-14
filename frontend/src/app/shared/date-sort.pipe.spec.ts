aa7afd90b1f565c8529ee4e32d89524523323116
Jakob Lierman <jakob.lierman@gmail.com>
2018-08-15 00:15:27 +0200

    Implementatie ReversePipe

--- frontend/src/app/shared/date-sort.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { DateSortPipe } from './date-sort.pipe';
-
-describe('DateSortPipe', () => {
-  it('create an instance', () => {
-    const pipe = new DateSortPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
--- frontend/src/app/shared/shared.module.ts
+++ frontend/src/app/shared/shared.module.ts
@@ -1,13 +1,13 @@
 import {BackButtonComponent} from "./back-button/back-button.component";
 import {NgModule} from "@angular/core";
 import {CommonModule} from "@angular/common";
-import {DateSortPipe} from "./date-sort.pipe";
+import {ReversePipe} from "./reverse.pipe";
 import {NavUserComponent} from "./nav-user/nav-user.component";
 
 @NgModule({
   imports: [CommonModule],
-  declarations: [BackButtonComponent, DateSortPipe, NavUserComponent],
-  exports: [BackButtonComponent, DateSortPipe, NavUserComponent]
+  declarations: [BackButtonComponent, ReversePipe, NavUserComponent],
+  exports: [BackButtonComponent, ReversePipe, NavUserComponent]
 })
 export class SharedModule {
 }
